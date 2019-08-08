3f076b5198eb59d9a5ad2a11421b14cc1da23675
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-08-08 16:30:22 -0700

    Initial Commit

---
+++ SwiftTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTableView
+//
+//  Created by user on 8/8/19.
+//  Copyright © 2019 Alphonso Sensley II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
