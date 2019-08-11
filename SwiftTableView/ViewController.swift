b978092173c78f619ce0be70d6e33c4252b5c631
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-08-10 18:29:44 -0700

    Practice tableView in Swift

--- SwiftTableView/ViewController.swift
+++ SwiftTableView/ViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var tableView: UITableView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+           tableView = UITableView(frame: CGRect(x: 0, y: 400, width: 400, height: 800))
+           tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+           tableView.delegate = self
+           tableView.dataSource = self
+           self.view.addSubview(tableView)
+       }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel!.text = "Hello World"
+        return cell
+    }
+    
+
+   
 
 
 }
---
+++ SwiftTableView/myCell.swift
@@ -0,0 +1,15 @@
+//
+//  myCell.swift
+//  SwiftTableView
+//
+//  Created by user on 8/8/19.
+//  Copyright © 2019 Alphonso Sensley II. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCell: UITableViewCell {
+    
+
+}
